77a5edd385a53b51caf7128029a85e6b9087aff0
Deanna Lingum <lingumd@gmail.com>
2022-09-20 02:54:51 -0400

    Updating file.

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -13,7 +13,7 @@ L.circle([34.0522, -118.2437], {
     fillColor: '#ffffa1'
  }).addTo(map);
 
-// We create the tile layer that will be the background of our map.
+// We create the tile layer for the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
