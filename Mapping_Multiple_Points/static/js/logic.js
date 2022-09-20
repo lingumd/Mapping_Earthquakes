c9c7a9a71f6b770ac9d238981f82803975878563
Deanna Lingum <lingumd@gmail.com>
2022-09-20 02:51:19 -0400

    Updating file.

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -5,8 +5,6 @@ console.log("working");
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 //  Add a marker to the map for Los Angeles, California.
-// let marker = L.marker([34.0522, -118.2437]).addTo(map);
-
 L.circle([34.0522, -118.2437], {
     radius: 300,
     color: "black",
@@ -17,9 +15,6 @@ L.circle([34.0522, -118.2437], {
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    // id: 'mapbox/streets-v11',
-    // tileSize: 512,
-    // zoomOffset: -1,
     accessToken: API_KEY
 });
 // Then we add our 'graymap' tile layer to the map.
